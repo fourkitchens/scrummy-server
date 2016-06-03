f39afcfe65fdab0fee4ba7b7567b2f7b7be22cd2
Flip Actual <flipactual@gmail.com>
2016-06-03 14:30:51 -0500

    document app

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const getFormattedEntityName = require('./util/getFormattedEntityName');
 const getUniqueFormattedEntityName = require('./util/getUniqueFormattedEntityName');
 
 class Scrummy {
+  /**
+   * constructor
+   *   Creates a new Scrummy server.
+   *
+   *   Starts a websocket server on the port specified in config, creates an empty bucket for
+   *   storing game related data, specifies which methods are available for invocation via websocket
+   *   message, and sets up websocket message handling.
+   *
+   * @return {undefined}
+   */
   constructor() {
     this.wss = server(config.get('port'));
     this.points = config.get('points');
@@ -17,6 +27,15 @@ class Scrummy {
       'placeVote',
     ];
   }
+  /**
+   * setupMessageHandling
+   *   Sets up listeners for messages once a connection to the websocket server has been made.
+   *
+   *   If the message type is one of the exposed methods, it is executed with the parsed message and
+   *   the websocket as arguments. Otherwise, the message is rejected.
+   *
+   * @return {undefined}
+   */
   setupMessageHandling() {
     this.wss.on('connection', ws => {
       ws.on('message', message => {
@@ -31,18 +50,54 @@ class Scrummy {
       });
     });
   }
+  /**
+   * shutdown
+   *   Shuts down the websocket server.
+   *
+   * @return {undefined}
+   */
   shutdown() {
     this.wss.close();
   }
+  /**
+   * broadcast
+   *   Sends the provided message to all matching clients.
+   *
+   * @param {String} data
+   *   The message to send.
+   * @param {Array} clients
+   *   The clients to send the message to.
+   * @return {undefined}
+   */
   broadcast(data, clients) {
     clients.forEach(client => client.ws.send(data));
   }
+  /**
+   * handleInvalidMessage
+   *   Sends an error to the client informing them that their message cannot be handled.
+   *
+   * @param {Object} data
+   *   The received message in Object form.
+   * @param {Object} ws
+   *   The client to send the message to.
+   * @return {undefined}
+   */
   handleInvalidMessage(data, ws) {
     ws.send(JSON.stringify({
       type: 'error',
       message: `${data.type} is not a message type Scrummy is prepared for!`,
     }));
   }
+  /**
+   * signIn
+   *   Signs a user in if they have provided a valid username.
+   *
+   * @param {Object} data
+   *   The signIn message from the client.
+   * @param {Object} ws
+   *   The websocket to respond to.
+   * @return {undefined}
+   */
   signIn(data, ws) {
     const requestedGame = getFormattedEntityName(data.game)
       || generateGameName(Object.keys(this.bucket));
@@ -67,8 +122,8 @@ class Scrummy {
     }
     this.bucket[requestedGame].clients.push({
       nickname,
-      ws,
       game: requestedGame,
+      ws,
     });
     this.bucket[requestedGame].users.push({
       nickname,
@@ -88,6 +143,16 @@ class Scrummy {
     }), this.bucket[requestedGame].clients);
     logger(`added user ${nickname} to ${requestedGame}\n`);
   }
+  /**
+   * placeVote
+   *   Places a vote on behalf of a client if the vote and game are valid.
+   *
+   * @param {Object} data
+   *   The signIn message from the client.
+   * @param {Object} ws
+   *   The websocket to respond to.
+   * @return {undefined}
+   */
   placeVote(data, ws) {
     if (!this.bucket[data.game]) {
       ws.send(JSON.stringify({
